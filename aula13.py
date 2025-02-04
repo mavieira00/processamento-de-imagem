49f5029d89d80c531800380c7c0cd78282089f06
Maria Vitória <142701083+mavieira00@users.noreply.github.com>
2025-02-04 11:29:29 -0300

    Add files via upload

--- aula13.py
+++ aula13.py
@@ -5,16 +5,33 @@ Created on Tue Jan 28 15:27:44 2025
 @author: 18798221728
 """
 
-entrada = 'ossos.jpg'
+# entrada = 'ossos.jpg'
 
 import cv2
 import numpy
 import mfmv
 
-imagem = cv2.imread(entrada)
-cinza = mfmv.tonsCinza(imagem)
-histCinza = mfmv.histograma(cinza, "grey")
-mfmv.plotGraf(histCinza, 'grey', "tons de cinza")  
+# imagem = cv2.imread(entrada)
+# cinza = mfmv.tonsCinza(imagem)
+# histCinza = mfmv.histograma(cinza, "grey")
+# mfmv.plotGraf(histCinza, 'grey', "tons de cinza")  
+
+matriz = numpy.array([
+    [255, 255, 255, 255, 0, 0, 0],
+    [255, 255, 255, 255, 0, 0, 0],
+    [255, 255, 255, 255, 0, 0, 0],
+    [255, 255, 255, 255, 255, 255, 255],
+    [255, 255, 255, 255, 255, 255, 255],
+    [255, 255, 255, 255, 255, 255, 255],
+    [255, 255, 255, 255, 255, 255, 255]
+]) 
+
+imagemOriginal = matriz.astype(numpy.uint8)
+
+matriz_redimensionada = cv2.resize(matriz, (70, 70), interpolation=cv2.INTER_NEAREST)
+
+matriz_uint8 = matriz_redimensionada.astype(numpy.uint8)
+imagem = matriz_uint8
 
 
 # matriz = numpy.zeros((10,10), dtype=int) #cria uma matriz toda preta
@@ -55,37 +72,74 @@ print('tamanho esperado:', (imagem.shape[1] * imagem.shape[0]))
 
 
 
-mascara33 = numpy.full((3, 3), 1)
+# mascara33 = numpy.full((3, 3), 1)
 
-# mascara33 = numpy.array([[0, 1, 0],
-#                     [1, 1, 1],
-#                     [0, 1, 0]])
+mascara33 = numpy.array([[0, 1, 0],
+                    [1, 1, 1],
+                    [0, 1, 0]])
 
 #--------------------------------------------------------------------------------------------------------#
 #-----------------------------Aula 13: Extração de Componentes Conectados--------------------------------#
 #--------------------------------------------------------------------------------------------------------#
 
-PretoBranco = mfmv.PBimg(cinza, 180)
+# PretoBranco = mfmv.PBimg(cinza, 180)
 
-posicao = mfmv.acharBuraco(PretoBranco)
-print("posicao inicial ", posicao)
+# posicao = mfmv.acharBuraco(PretoBranco)
+# print("posicao inicial ", posicao)
 
-X = numpy.zeros_like(PretoBranco)
-X[posicao] = 255
+# X = numpy.zeros_like(PretoBranco)
+# X[posicao] = 255
 
-while True :
-    XProx = mfmv.dilatacao(X, mascara33, 1)
-    XProx = numpy.where((XProx == 255) & (PretoBranco == 255), 255, 0).astype(numpy.uint8)
-    if numpy.array_equal(X, XProx):
-        break
-    X = XProx
-    # cv2.imshow("X", X)
-    # cv2.waitKey(0)
+# while True :
+#     XProx = mfmv.dilatacao(X, mascara33, 1)
+#     XProx = numpy.where((XProx == 255) & (PretoBranco == 255), 255, 0).astype(numpy.uint8)
+#     if numpy.array_equal(X, XProx):
+#         break
+#     X = XProx
+#     # cv2.imshow("X", X)
+#     # cv2.waitKey(0)
 
     
-imagemFinal = X
+# imagemFinal = X
    
+# cv2.imshow("imagem original", imagem)
+# cv2.imshow("imagem preta e branca", PretoBranco)
+# cv2.imshow("imagem final", imagemFinal)
+# cv2.waitKey(0)
+
+#--------------------------------------------------------------------------------------------------------#
+#--------------------------------------Aula 13: Esqueletização-------------------------------------------#
+#--------------------------------------------------------------------------------------------------------#
+
+PretoBranco = mfmv.PBimg(imagem, 150)
+
+def esqueletizacao(imagem , e_estruturante, k):
+    caveira = numpy.zeros((imagem.shape[0] , imagem.shape[1]), dtype=imagem.dtype)
+    abertura1 = mfmv.abertura(imagem, e_estruturante, 1)
+    diferenca1 = imagem - abertura1
+    caveira = diferenca1
+    
+    while True:
+        erosaok = mfmv.erosao(imagem, e_estruturante, k)
+        aberturak = mfmv.abertura(erosaok, e_estruturante, 1)
+        diferencak = erosaok - aberturak 
+        
+        caveira = numpy.maximum(caveira, diferencak)
+        
+        if numpy.all(aberturak == 0):
+            break
+        
+        k = k + 1
+        
+    return caveira
+
+caveira = esqueletizacao(PretoBranco, mascara33, 1)
+inversao = mfmv.inverterPB(PretoBranco)
+sobrepor = numpy.maximum(caveira, inversao) 
+
 cv2.imshow("imagem original", imagem)
-cv2.imshow("imagem preta e branca", PretoBranco)
-cv2.imshow("imagem final", imagemFinal)
-cv2.waitKey(0)
+cv2.imshow("imagem esqueletica", esqueleto)
+cv2.imshow("imagem final", sobrepor)
+cv2.waitKey(0) 
+    
+
